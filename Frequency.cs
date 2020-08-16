4eabb121c81f32a89ce427609b6e306a6c92830b
bored-user <joao.guimaraes.alves.vaz@gmail.com>
2020-08-16 20:03:41 -0300

    Frequency now working perfectly

--- Frequency.cs
+++ Frequency.cs
@@ -1,7 +1,30 @@
+using System.Collections.Generic;
+
 namespace Statistics
 {
     public class Frequency
     {
-        
+        public static string Index(long[] nums)
+        {
+            nums = Order.Index(nums);
+            Dictionary<long, long> frequency = new Dictionary<long, long>();
+            string res = "";
+
+            foreach (int num in nums)
+            {
+                if (!frequency.ContainsKey(num))
+                    frequency[num] = 0;
+
+                frequency[num]++;
+            }
+
+            foreach(var (key, val) in frequency)
+            {
+                res += $"Number {key} appeared {val} ";
+                res += val > 1 ? "times.\n" : "time.\n";
+            }
+
+            return res;
+        }
     }
 }

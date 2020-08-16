fe1fd862b46bb728ff4f1d3b09b53c76ec602592
bored-user <joao.guimaraes.alves.vaz@gmail.com>
2020-08-16 20:04:31 -0300

    Implemented median calculus

--- Median.cs
+++ Median.cs
@@ -2,9 +2,12 @@ namespace Statistics
 {
     public class Median
     {
-        public static void Index(long[] nums)
+        public static double Index(long[] nums)
         {
+            nums = Order.Index(nums);
+            int half = nums.Length / 2;
 
+            return nums.Length % 2.0 != 0 ? nums[half] : (nums[half] + nums[half - 1]) / 2.0;
         }
     }
 }

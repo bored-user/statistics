03edb75714d1d2b16b3d784c31a0b63ff1c5a79e
bored-user <joao.guimaraes.alves.vaz@gmail.com>
2020-08-17 19:43:18 -0300

    Add variance & standard error. Returning as string

--- Program.cs
+++ Program.cs
@@ -8,12 +8,12 @@ namespace Statistics
     {
         public static void Main(string[] args)
         {
-            Console.Write("Howdy!\nWhat you'd like to calculate?\n\n0). Order numbers in ascending order\n1). Mean\n2). Median\n3). Mode\n4). Amplitude\n5). Standard deviation\n6). Frequency\n\n> Your choice: ");
+            Console.Write("Howdy!\nWhat you'd like to calculate?\n\n0). Order numbers in ascending order\n1). Mean\n2). Median\n3). Mode\n4). Amplitude\n5). Standard deviation\n6). Frequency\n7). Standard error\n8). Variance\n\n> Your choice: ");
 
             try
             {
                 string input = Console.ReadLine();
-                string[] range = new string[] { "0", "1", "2", "3", "4", "5", "6" };
+                string[] range = new string[] { "0", "1", "2", "3", "4", "5", "6", "7", "8" };
                 if (!range.Contains(input))
                     throw new IndexOutOfRangeException($"Expecting number in range [{range[0]}..{range.Last()}]. Got '{input}'");
 
@@ -35,33 +35,39 @@ namespace Statistics
             }
         }
 
-        private static dynamic Caller(string input, long[] nums)
+        private static string Caller(string input, long[] nums)
         {
             switch (input)
             {
                 case "0":
-                    return Order.ToString(Order.Index(nums));
+                    return $"Numbers in ascending order: {Order.ToString(Order.Index(nums))}";
 
                 case "1":
-                    return Mean.Index(nums);
+                    return $"Mean: {Mean.Index(nums)}";
 
                 case "2":
-                    return Median.Index(nums);
+                    return $"Median: {Median.Index(nums)}";
 
                 case "3":
-                    return Mode.Index(nums);
+                    return $"Mode: {Mode.Index(nums)}";
 
                 case "4":
-                    return Amplitude.Index(nums);
+                    return $"Amplitude: {Amplitude.Index(nums)}";
 
                 case "5":
-                    return Standard_Deviation.Index(nums);
+                    return $"Standard deviation: {Standard_Deviation.Index(nums)}";
 
                 case "6":
                     return Frequency.Index(nums);
 
+                case "7":
+                    return $"Standard error: {Standard_Error.Index(nums)}";
+
+                case "8":
+                    return $"Variance: Variance.Index(nums)";
+
                 default:
-                    return -1;
+                    return "404 - Not found";
             }
         }
     }
